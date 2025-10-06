33645e5876fdb9e6a85ef33a4a0850beea4f06da
Hendrix Gullixson <hgullixson@gmail.com>
2025-10-06 17:55:08 -0500

    tab animation fix

--- js/main.js
+++ js/main.js
@@ -122,30 +122,20 @@
         window.addEventListener('scroll', navHighlight);
 
         function navHighlight() {
-        
-            // Get current scroll position
-            let scrollY = window.pageYOffset;
-        
-            // Loop through sections to get height(including padding and border), 
-            // top and ID values for each
-            sections.forEach(function(current) {
-                const sectionHeight = current.offsetHeight;
-                const sectionTop = current.offsetTop - 50;
-                const sectionId = current.getAttribute('id');
-            
-               /* If our current scroll position enters the space where current section 
-                * on screen is, add .current class to parent element(li) of the thecorresponding 
-                * navigation link, else remove it. To know which link is active, we use 
-                * sectionId variable we are getting while looping through sections as 
-                * an selector
-                */
-                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                    document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.add('current');
-                } else {
-                    document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.remove('current');
-                }
-            });
+    let scrollY = window.pageYOffset;
+
+    sections.forEach(function(current) {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - window.innerHeight * 0.3; // start a bit later
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight * 0.5) {
+            document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.add('current');
+        } else {
+            document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.remove('current');
         }
+    });
+}
 
     }; // end ssScrollSpy
 
